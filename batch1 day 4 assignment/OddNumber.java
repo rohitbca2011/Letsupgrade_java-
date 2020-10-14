1b03b3f375ae5c50d2e128f57f0671e5c1fe10ad
rohitbca2011 <69254804+rohitbca2011@users.noreply.github.com>
2020-10-14 11:27:57 +0530

    Create OddNumber.java

---
+++ batch1 day 4 assignment/OddNumber.java
@@ -0,0 +1,15 @@
+public class OddNumber{  
+public static void main(final String args[]){
+
+   int [] arr = new int [] {1,2,5,6,3,7};
+
+    System.out.println("Odd Numbers:");  
+
+     for(int i=0;i<arr.length;i++){  
+         if(arr[i]%2!=0){  
+
+            System.out.println(arr[i]); 
+         }
+        }
+    }   
+}
